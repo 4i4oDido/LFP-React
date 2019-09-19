cda6c833b8f459cfce4ff1f5d44b1e4e2c432e67
Dilyan Glozhenski <dglozhenski@hhogdev.com>
2019-09-19 09:28:13 +0300

    db update

--- src/Components/Header/Scorebar/GameResult.js
+++ src/Components/Header/Scorebar/GameResult.js
@@ -4,8 +4,8 @@ const GameResult = (props)=> {
     return (
         <div className="GameResult">
             <div className="Date">{props.date}<span>{props.hour}</span></div>
-            <div className="Team HomeTeam">{props.homeTeamName}<span>{props.homeTeamScore}</span></div>
-            <div className="Team AwayTeam">{props.awayTeamName}<span>{props.awayTeamScore}</span></div>
+            <div className="Team HomeTeam"><img src={props.homeTeam.logo} alt="logo"/>{props.homeTeamName}<span>{props.homeTeamScore}</span></div>
+            <div className="Team AwayTeam"><img src={props.awayTeam.logo} alt="logo"/>{props.awayTeamName}<span>{props.awayTeamScore}</span></div>
         </div>
     )
 }
